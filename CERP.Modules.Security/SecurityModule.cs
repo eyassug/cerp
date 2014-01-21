169183538e6b492a1d0f60be247bdb9111ddf75e
Eyassu Getachew <eyassug@gmail.com>
2014-01-21 21:12:20 +0300

    Added EntityFramework package to CERP.Modules.Security and CERP.Modules.Security.Tests project

---
+++ CERP.Modules.Security/SecurityModule.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Modules;
+
+namespace CERP.Modules.Security
+{
+    class SecurityModule : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
