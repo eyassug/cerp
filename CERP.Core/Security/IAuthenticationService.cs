f99fcd6cf8e95e302262d4bcbf992c1c74a11ff1
Eyassu Getachew <eyassug@gmail.com>
2014-01-21 21:12:43 +0300

    Added IAuthenticationService interface to CERP.Core

---
+++ CERP.Core/Security/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿namespace CERP.Core.Security
+{
+    /// <summary>
+    /// Authenticates given credentials against a list of users
+    /// </summary>
+    interface IAuthenticationService
+    {
+        void Authenticate(string username, string password);
+    }
+}
