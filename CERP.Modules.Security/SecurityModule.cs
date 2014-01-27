7387ef108e447c3df4c05b185698ba152ca73f5d
Eyassu Getachew <eyassug@gmail.com>
2014-01-27 20:07:10 +0300

    Updated SecurityModule to accept an instance of SecurityModuleSettings

--- CERP.Modules.Security/SecurityModule.cs
+++ CERP.Modules.Security/SecurityModule.cs
@@ -1,9 +1,19 @@
-﻿using Ninject.Modules;
+﻿using System;
+using Ninject.Modules;
 
 namespace CERP.Modules.Security
 {
     class SecurityModule : NinjectModule
     {
+        private readonly SecurityModuleSettings _settings;
+
+        public SecurityModule(SecurityModuleSettings settings)
+        {
+            if(settings == null)
+                throw new ArgumentNullException("settings");
+            _settings = settings;
+        }
+
         public override void Load()
         {
             throw new System.NotImplementedException();
