ccfbd839efb140dd126b1b7f537853ca7208dc99
Eyassu Getachew <eyassug@gmail.com>
2014-01-28 17:17:31 +0300

    Implemented DatabaseAuthenticationService.Authenticate() method

--- CERP.Modules.Security/Services/DatabaseAuthenticationService.cs
+++ CERP.Modules.Security/Services/DatabaseAuthenticationService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
+using Apex.Common.Data;
 using CERP.Core.Security;
+using CERP.Modules.Security.Repository;
 
 namespace CERP.Modules.Security.Services
 {
@@ -12,6 +15,20 @@ namespace CERP.Modules.Security.Services
     /// </summary>
     class DatabaseAuthenticationService : IAuthenticationService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserRepository _userRepository;
+
+        public DatabaseAuthenticationService()
+            : this(new SecurityDataContext())
+        {
+        }
+
+        private DatabaseAuthenticationService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = new UserRepository(_unitOfWork);
+            
+        }
         /// <summary>
         /// Creates a session and sets it to Thread.Current principal. Throws InvalidCredentialsException upon error
         /// </summary>
@@ -19,7 +36,13 @@ namespace CERP.Modules.Security.Services
         /// <param name="password">Password</param>
         public void Authenticate(string username, string password)
         {
-            throw new NotImplementedException();
+            // TODO: Sanitise parameters
+            // TODO: Encrypt password
+            var user = _userRepository.FindSingle(m => m.Username == username);
+            if(user == null)
+                throw new InvalidCredentialException("The supplied username is incorrect.");
+            if(user.Password != password)
+                throw new InvalidCredentialException("The supplied password is incorrect.");
         }
     }
 }
