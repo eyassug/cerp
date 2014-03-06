fedbc61e0cf6159f1a10aa1d37d7071295cc7c5a
Eyassu Getachew <eyassug@gmail.com>
2014-03-06 20:14:48 +0300

    Added DataManagement Module and Unit Test

---
+++ CERP.Modules.DataManagement.Tests/DataManagementTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CERP.Modules.DataManagement.Tests
+{
+    [TestClass]
+    public class DataManagementTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
