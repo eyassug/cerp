aec8c8bf08118c7f60209e0474ddf0eef90b94bb
Eyassu Getachew <eyassug@gmail.com>
2014-03-27 01:36:26 +0300

    Renamed DataManagementTest class

---
+++ CERP.Modules.DataManagement.Tests/ProductServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CERP.Modules.DataManagement.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CERP.Modules.DataManagement.Tests
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        readonly IProductService productService;
+        readonly private IProductCategoryService productCategoryService;
+
+
+        [TestMethod]
+        public void AddNewProductWithoutAManufacturerTest_ShouldThrowException()
+        {
+            var category = new ProductCategory {Name = "Laptop"};
+            productCategoryService.AddNewProductCategory(category);
+            var newProduct = new Product
+                                 {
+                                     Name = "Toshiba L755-4905",
+                                     IsActive = true,
+                                     ProductCategory = category,
+                                     ProductCode = "TL755"
+                                 };
+
+            productService.AddProduct(newProduct);
+        }
+    }
+}
