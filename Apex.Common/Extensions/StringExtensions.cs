c0f59820fde4406414a2cb8f5aeb655a65623160
Eyassu Getachew <eyassug@gmail.com>
2014-03-28 20:46:15 +0300

    Added StringExtensions method

---
+++ Apex.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        
+        public static string Sanitise(this string unsanitisedString)
+        {
+            if (unsanitisedString.Sanitised())
+                return unsanitisedString;
+            unsanitisedString = unsanitisedString.Replace('\'',' ');
+            return unsanitisedString;
+        }
+
+        public static bool Sanitised(this string unsanitisedString)
+        {
+            // TODO: Match string against a regex
+            return !unsanitisedString.Contains("'");
+        }
+    }
+}
