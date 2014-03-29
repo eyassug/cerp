ac9613b6921cf1797a25aee152eb7f8263bcdeea
Samuel habtewold <andeabesha@gmail.com>
2014-03-29 03:14:54 +0300

    Added Location model

---
+++ CERP.Modules.Models/Inventory/Location.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CERP.Models.Inventory
+{
+    [Table("Location", Schema = "Inventory")]
+    public class Location
+    {
+        [Key]
+        public int LocationID { get; set; }
+        public string Name { get; set; }
+        public int LocationNode { get; set; }
+    }
+}
