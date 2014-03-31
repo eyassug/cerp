153b741c2a0d743f380618fca150162d65fc6440
Samuel habtewold <andeabesha@gmail.com>
2014-03-31 15:27:11 +0300

    Added product repository

---
+++ CERP.Modules.DataManagement/Services/Repository/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Apex.Common.Data;
+using CERP.Modules.DataManagement.DataAccess;
+using CERP.Models.Inventory;
+
+namespace CERP.Modules.DataManagement.Repository
+{
+    class ProductRepository : EntityFrameworkRepository<DataManagementContext,Product>
+    {
+        public ProductRepository(IUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+    }
+}
