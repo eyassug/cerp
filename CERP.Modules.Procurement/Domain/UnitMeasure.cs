0216b4c8f287b0f46cb03027cc71af4daa910652
Samuel habtewold <andeabesha@gmail.com>
2014-03-31 15:24:39 +0300

    Added unit measure model to domain

--- CERP.Modules.Procurement/Domain/UnitMeasure.cs
+++ CERP.Modules.Procurement/Domain/UnitMeasure.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CERP.Modules.Procurement.Domain
+﻿
+namespace CERP.Modules.DataManagement.Domain
 {
     public class UnitMeasure
     {
+        public string UnitMeasureCode { get; set; }
+        public string Name { get; set; }
     }
 }
