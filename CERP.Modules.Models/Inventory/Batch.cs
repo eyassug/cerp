9ff0043d5875c214151f70e3e357dc5313161a22
Eyassu Getachew <eyassug@gmail.com>
2014-04-05 20:18:34 +0300

    Added Batch and InventoryAccount Data Models

---
+++ CERP.Modules.Models/Inventory/Batch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CERP.Models.Inventory
+{
+    [Table("Batch", Schema = "Inventory")]
+    public class Batch
+    {
+        public Batch()
+        {
+            BatchIdentifier = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid BatchIdentifier { get; set; }
+        public string SerialNumber { get; set; }
+    }
+}
---
+++ CERP.Modules.Models/Inventory/InventoryAccount.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CERP.Models.Inventory
+{
+    [Table("InventoryAccount", Schema = "Inventory")]
+    public class InventoryAccount
+    {
+        [Key]
+        public int InventoryAccountID { get; set; }
+        public string Name { get; set; }
+        public string InventoryAccountCode { get; set; }
+    }
+}
