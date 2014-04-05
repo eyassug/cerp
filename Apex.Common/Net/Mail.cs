10d5fe5b17ad4ee7031672a2e38834ecfc389ccb
Eyassu Getachew <eyassug@gmail.com>
2014-04-05 20:16:13 +0300

    Added code comment for Mail

--- Apex.Common/Net/Mail.cs
+++ Apex.Common/Net/Mail.cs
@@ -8,7 +8,12 @@ namespace Apex.Common.Net
     public class Mail
     {
         private IList<Attachment> _attachments;
-
+        /// <summary>
+        /// Constructs a new Mail object
+        /// </summary>
+        /// <param name="subject">Subject of the email</param>
+        /// <param name="body">Body of the message</param>
+        /// <param name="attachments">Optional file locations</param>
         public Mail(string subject,string body, params string[] attachments)
         {
             Subject = subject;
@@ -18,10 +23,18 @@ namespace Apex.Common.Net
                 Attachments.Add(new Attachment(attachment));
             }
         }
-        
+        /// <summary>
+        /// Subject of the mail
+        /// </summary>
         public string Subject { get; private set; }
+        /// <summary>
+        /// Body of the mail
+        /// </summary>
         public string Body { get; private set; }
 
+        /// <summary>
+        /// Attachments
+        /// </summary>
         public IList<Attachment> Attachments
         {
             get
