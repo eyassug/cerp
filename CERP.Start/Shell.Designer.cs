7c08139c3c0c25feca31a04f724cfca8d11056dd
Eyassu Getachew <eyassug@gmail.com>
2014-04-08 01:01:32 +0300

    Added CERP.Start project

---
+++ CERP.Start/Shell.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CERP.Start
+{
+    partial class Shell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.btnEmployees = new DevExpress.XtraBars.BarButtonItem();
+            this.hrPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.employeesPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.tabManager = new DevExpress.XtraTabbedMdi.XtraTabbedMdiManager();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabManager)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbon
+            // 
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.ExpandCollapseItem,
+            this.btnEmployees});
+            this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.MaxItemId = 2;
+            this.ribbon.Name = "ribbon";
+            this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.hrPage});
+            this.ribbon.Size = new System.Drawing.Size(442, 144);
+            this.ribbon.StatusBar = this.ribbonStatusBar;
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Caption = "Employees";
+            this.btnEmployees.Id = 1;
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.BtnEmployeesClick);
+            // 
+            // hrPage
+            // 
+            this.hrPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.employeesPageGroup});
+            this.hrPage.Name = "hrPage";
+            this.hrPage.Text = "Human Resources";
+            // 
+            // employeesPageGroup
+            // 
+            this.employeesPageGroup.ItemLinks.Add(this.btnEmployees);
+            this.employeesPageGroup.Name = "employeesPageGroup";
+            this.employeesPageGroup.Text = "Employees";
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 418);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.ribbon;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(442, 31);
+            // 
+            // tabManager
+            // 
+            this.tabManager.MdiParent = this;
+            // 
+            // Shell
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 449);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbon);
+            this.IsMdiContainer = true;
+            this.Name = "Shell";
+            this.Ribbon = this.ribbon;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Shell";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabManager)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
+        private DevExpress.XtraBars.Ribbon.RibbonPage hrPage;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup employeesPageGroup;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraBars.BarButtonItem btnEmployees;
+        private DevExpress.XtraTabbedMdi.XtraTabbedMdiManager tabManager;
+    }
+}
---
+++ CERP.Start/Shell.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+using CERP.Modules.HumanResources.Controls.EmployeeManagement;
+using DevExpress.XtraBars;
+
+namespace CERP.Start
+{
+    public partial class Shell : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEmployeesClick(object sender, ItemClickEventArgs e)
+        {
+            OpenOrActivateTab(new AddEmployeeShell());
+        }
+
+        #region Tab Manager
+        private void OpenOrActivateTab<TForm>(TForm window) where TForm : Form, new()
+        {
+            // Check if a window is already open
+            foreach (var mdiChild in MdiChildren.Where(mdiChild => mdiChild.GetType() == typeof(TForm)))
+            {
+                mdiChild.Activate();
+                return;
+            }
+            window.MdiParent = this;
+            window.Show();
+        }
+
+        #endregion
+    }
+}
