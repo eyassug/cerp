5f6aaa1e843a8fec01c4803a4cd564364ddb50fc
Eyassu Getachew <eyassug@gmail.com>
2014-04-10 00:14:51 +0300

    Added EmailConsole application to send queued payslips

---
+++ CERP.Modules.HumanResources.EmailConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using CERP.Modules.HumanResources.Controls.Payroll.Reports;
+using CERP.Modules.HumanResources.Controls.Payroll.ViewModels;
+
+namespace CERP.Modules.HumanResources.EmailConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var slip = new PaySlip
+                           {
+                               PaySlipInformation = new EmployeePayrollViewModel()
+                                                        {
+                                                            Name = "Eyassu",
+                                                            GrossSalary = 10,
+                                                            IncomeTax = 100
+                                                        }
+                           };
+            slip.ExportToPdf(@"E:\abc.pdf");
+        }
+    }
+}
