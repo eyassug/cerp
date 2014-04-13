075e8440bc940ea59cdb079a8c7a071e0a677761
Eyassu Getachew <eyassug@gmail.com>
2014-04-14 00:29:26 +0300

    Added EntityFramework & Ninject packages to CERP.Start

--- CERP.Start/Program.cs
+++ CERP.Start/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CERP.Modules.HumanResources;
+using Ninject;
 
 namespace CERP.Start
 {
     static class Program
     {
+        public static IKernel Kernel;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +19,16 @@ namespace CERP.Start
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            LoadModules();
             Application.Run(new Shell());
+        }
+
+        static void LoadModules()
+        {
+            Kernel = new StandardKernel(new HumanResourcesModule(new HumanResourcesModuleConfiguration()
+                                                                     {
+                                                                         ConnectionString = @"Data Source=.\SQLSERVER;Initial Catalog=CERP;Integrated Security=True;"
+                                                                     }));
         }
     }
 }
