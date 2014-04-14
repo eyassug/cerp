7830cb14c647ae41038531fa441c46eb435e103f
Eyassu Getachew <eyassug@gmail.com>
2014-04-14 16:56:05 +0300

    Added NewPayrollShell control

---
+++ CERP.Modules.HumanResources.Controls/Payroll/NewPayrollShell.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CERP.Modules.HumanResources.Controls.Payroll
+{
+    partial class NewPayrollShell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.payrollGridView = new CERP.Modules.HumanResources.Controls.Payroll.Views.PayrollGridView();
+            this.payrollHeaderView = new CERP.Modules.HumanResources.Controls.Payroll.Views.PayrollHeaderView();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.payrollGridView);
+            this.layoutControl1.Controls.Add(this.payrollHeaderView);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(863, 316);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1";
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem2,
+            this.emptySpaceItem1,
+            this.layoutControlItem1});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(863, 316);
+            this.layoutControlGroup1.Text = "layoutControlGroup1";
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.CustomizationFormText = "emptySpaceItem1";
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 99);
+            this.emptySpaceItem1.MaxSize = new System.Drawing.Size(0, 10);
+            this.emptySpaceItem1.MinSize = new System.Drawing.Size(10, 10);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(843, 10);
+            this.emptySpaceItem1.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.emptySpaceItem1.Text = "emptySpaceItem1";
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // payrollGridView
+            // 
+            this.payrollGridView.Location = new System.Drawing.Point(12, 121);
+            this.payrollGridView.Name = "payrollGridView";
+            this.payrollGridView.Size = new System.Drawing.Size(839, 183);
+            this.payrollGridView.TabIndex = 6;
+            // 
+            // payrollHeaderView
+            // 
+            this.payrollHeaderView.Location = new System.Drawing.Point(12, 12);
+            this.payrollHeaderView.Name = "payrollHeaderView";
+            this.payrollHeaderView.PayrollInformation = null;
+            this.payrollHeaderView.Size = new System.Drawing.Size(839, 95);
+            this.payrollHeaderView.TabIndex = 5;
+            this.payrollHeaderView.CreatePayrollClick += new System.EventHandler(this.PayrollHeaderViewCreatePayrollClick);
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.payrollHeaderView;
+            this.layoutControlItem2.CustomizationFormText = "layoutControlItem2";
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(843, 99);
+            this.layoutControlItem2.Text = "layoutControlItem2";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextToControlDistance = 0;
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.payrollGridView;
+            this.layoutControlItem1.CustomizationFormText = "layoutControlItem1";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 109);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(843, 187);
+            this.layoutControlItem1.Text = "layoutControlItem1";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextToControlDistance = 0;
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // NewPayrollShell
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 316);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "NewPayrollShell";
+            this.Text = "New Payroll";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private Views.PayrollHeaderView payrollHeaderView;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private Views.PayrollGridView payrollGridView;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+    }
+}
---
+++ CERP.Modules.HumanResources.Controls/Payroll/NewPayrollShell.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CERP.Modules.HumanResources.Controls.Payroll.ViewModels;
+using CERP.Modules.HumanResources.Services;
+using DevExpress.XtraEditors;
+
+namespace CERP.Modules.HumanResources.Controls.Payroll
+{
+    public partial class NewPayrollShell : Form
+    {
+        private IPayrollService _payrollService;
+        public NewPayrollShell()
+        {
+            InitializeComponent();
+            _payrollService = new PayrollService();
+        }
+
+        private void PayrollHeaderViewCreatePayrollClick(object sender, EventArgs e)
+        {
+            var payrollInfo = payrollHeaderView.PayrollInformation;
+            if (payrollInfo == null)
+                XtraMessageBox.Show("Invalid Payroll Header");
+            LoadGrid(payrollInfo);
+        }
+
+        private void LoadGrid(PayrollInformationViewModel payrollInfo)
+        {
+            var payroll = _payrollService.CreateNew(payrollInfo.Period, payrollInfo.StartDate, payrollInfo.EndDate);
+            var gridDetails = from p in payroll.PayrollDetails
+                              select new EmployeePayrollViewModel
+                                         {
+                                             Name = string.Format("{0} {1}", p.Employee.FirstName, p.Employee.LastName),
+                                             Department = p.Employee.Department.Name,
+                                             GrossSalary = p.GrossSalary,
+                                             IncomeTax = p.IncomeTax,
+                                             PensionDeduction = p.Pension,
+                                             LoanDeduction = p.OtherDeductions,
+                                             ProvidentFund = 0
+                                         };
+            payrollGridView.PayrollDetails = gridDetails.ToList();
+        }
+    }
+}
