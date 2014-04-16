df072f7fd35ca3edb7056322888a087336284906
Eyassu Getachew <eyassug@gmail.com>
2014-04-16 14:38:50 +0300

    Fixed Table attribute on EmployeeDepartmentHistory Model class

--- CERP.Modules.Models/HumanResources/EmployeeDepartmentHistory.cs
+++ CERP.Modules.Models/HumanResources/EmployeeDepartmentHistory.cs
@@ -4,16 +4,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CERP.Models.HumanResources
 {
-    [Table("EmployeePayHistory", Schema = "HumanResources")]
+    [Table("EmployeeDepartmentHistory", Schema = "HumanResources")]
     public class EmployeeDepartmentHistory
     {
         [Key,Column(Order = 1)][ForeignKey("Employee")]
         public int EmployeeID { get; set; }
-        [Key, Column(Order = 1)]
+        [Key, Column(Order = 2)]
         public int DepartmentID { get; set; }
-        [Key, Column(Order = 1)]
+        [Key, Column(Order = 3)]
         public int ShiftID { get; set; }
-        [Key, Column(Order = 1)]
+        [Key, Column(Order = 4)]
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
 
